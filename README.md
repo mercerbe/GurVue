82671a44505e0616a7d00a00a123c75e589eefb1
Benjamin Mercer <mercerbe@gmail.com>
2018-12-01 22:56:52 -0500

    init, add element UI

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GurVue
-imgur clone built with VueJs 2.0
+imgur clone built with VueJs 2.0 using: 
+- vue cli3
+- vuex
+- lodash 
+- axios 
+
+
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    <Header></Header>
+  </div>
+</template>
+
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
+</style>
